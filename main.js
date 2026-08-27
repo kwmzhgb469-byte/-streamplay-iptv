c13a9a646bd6421a4245cf1a1eaac3006d44347e
kwmzhgb469-byte <kwmzhgb469@privaterelay.appleid.com>
2026-08-27 11:28:09 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>🎬 StreamPlay IPTV 2.0</h1>
+  <p>Bem-vindo ao StreamPlay IPTV!</p>
+  <p>Carregando aplicação...</p>
+`
+
+console.log('StreamPlay IPTV iniciando...')
